99ef2f236e4d0e1193cbd5a2bb1400bb14a4f7fc
kingpreyansh <preyanshdutta@gmail.com>
2022-02-11 11:44:30 -0500

    2.3, 2.4

--- 1. Sliding Window/3_smallest_subarray_with_sum.py
+++ 1. Sliding Window/3_smallest_subarray_with_sum.py
@@ -4,20 +4,22 @@
 # Return 0, if no such subarray exists.
 # Example input: [2, 1, 5, 2, 3, 2], S=7 
 # Leetcode Equivalent: https://leetcode.com/problems/minimum-size-subarray-sum/
-# Optimal solution: 
+# Optimal solution: O(n) The outer for loop runs for all elements and the inner while loop processes each element only once, therefore the time complexity of the algorithm will be O(N+N)O(N+N) which is asymptotically equivalent to O(N)O(N).
 
-def minSubArrayLen(array, s):
-  # We need variables for windowStart, windowEnd, windowSum and minWindow
-  # Each of these variables serve its duty and their tasks can be inferred from their variable name
-  windowSum, windowStart = 0.0, 0
-  minWindow = 999999
-  for windowEnd in range(len(array)):
-    windowSum = sum(array[windowStart: windowEnd])
-    while(sum(array[windowStart: windowEnd]) >= s):
-      minWindow = min(len(array[windowStart: windowEnd]), minWindow)
-      windowStart+=1
+def minSubArrayLen(self, array, s):
+  windowSum, windowStart = 0.0, 0 
+  minWindow = 9999999
 
+  for windowEnd in range(0, len(array)): # to increase the window size while we move forward
+    windowSum += array[windowEnd] # to increase the sum of the window
+    while(windowSum >= s): # keep decreasing the window and try to find the most minimal size that satisfies our condition
+      minWindow = min(windowEnd - windowStart + 1, minWindow) 
+      windowSum -= array[windowStart] # when the window slides the first element gets cut from the sum
+      windowStart += 1 # shifting the window over
+  
+  if minWindow == 9999999:
+    return 0
   return minWindow
 
 if __name__ == "__main__":
-  print(minSubArrayLen([2, 1, 5, 2, 3, 2], 7))
+  print(minSubArrayLen([2,3,1,2,4,3], 7))
--- 1. Sliding Window/4_longest_substring_with_K_distinct_characters.py
+++ 1. Sliding Window/4_longest_substring_with_K_distinct_characters.py
@@ -1,11 +1,29 @@
 # coding: utf-8
-# Problem:
-# Example input: 
-# Leetcode Equivalent:
-# Optimal solution: 
+# Problem: Given a string, find the length of the longest substring in it with no more than K distinct characters.
+# Example Input: String="araaci", K=2
+# Output: 4
+# Explanation: The longest substring with no more than '2' distinct characters is "araa".
+# Leetcode Equivalent: https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+# Optimal solution: O(n)
 
-def lengthOfLongestSubstringKDistinct():
-  return 1
+# TIP: whenever there is a question asking for distinct characters in a string, best to use a hashset/hashmap
+
+def lengthOfLongestSubstringKDistinct(s, k):
+  windowStart, maxLen = 0, 0 # using the sliding window approach, there's a windowStart and a maxLen (maximum length of the subarray)
+  charDict = {} # to keep a track of unique characters
+  for windowEnd in range(0, len(s)):  # iterate through the string
+    if s[windowEnd] not in charDict: # if the item in the string is not in the dict then add it to the dict
+      charDict[s[windowEnd]] = 0 
+    charDict[s[windowEnd]] += 1 # else increment the item
   
+    while(len(charDict) > k): # if the length of the dict is greater than k then slide the window
+      charDict[s[windowStart]] -= 1
+      if(charDict[s[windowStart]] == 0): # if there are no more characters then delete the key
+        del(charDict[s[windowStart]])
+      windowStart += 1
+    maxLen = max(maxLen, windowEnd - windowStart + 1)
+
+  return maxLen # return the maximum length through the whole iteration
+
 if __name__ == "__main__":
-  print('hello')
+  print(lengthOfLongestSubstringKDistinct("araaci", 2))
